9a4ef6f7725910be0b82b98d193e0e8fdc198c5c
Sam Val <samvalentine2811@gmail.com>
2021-03-05 01:07:04 +0700

    update

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 from requests import get
+import os
 
-API_KEY_IPSTACK = "c45007a38ef3d30a0855d1fa9be4bc6c"
+API_KEY_IPSTACK = os.environ.get('API_KEY_IPSTACK')
 ipstack_endpoint = "http://api.ipstack.com/"
 
 
-API_KEY_OPEN_WEATHER = "310c3d762791a9a38bd19a988224eea7"
-api_key = 0
+API_KEY_OPEN_WEATHER = os.environ.get('API_KEY_OPEN_WEATHER')
 def weather_from_cords(lat,lon):
 
     rs = get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}?lon={lon}&appid={API_KEY_OPEN_WEATHER}")
